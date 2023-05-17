a4119706572558766a283e245ac04e4892f53275
jonathan <jonifu06@gmail.com>
2023-05-17 13:54:32 +0200

    added /verify and /logout

--- routes/authentication.js
+++ routes/authentication.js
@@ -7,13 +7,26 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/login', function (req, res, next) {
-    if (req.body.email === 'test@gmail.com' && req.body.password === '123456') {
+    if (/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(req.body.email) && req.body.password === 'm295') {
         req.session.email = (req.body.email);
         req.session.password = (req.body.password);
-        res.sendStatus(201);
+        res.status(201).json('login was successful');
     } else {
-        res.sendStatus(401);
+        res.status(401).json('wrong email or password');
     };
+});
+
+router.get('/verify', function (req, res, next) {
+    if (/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(req.session.email) && req.session.password === 'm295') {
+        res.status(200).json('you are logged in');
+    } else {
+        res.status(401).json('you are not logged in');
+    };
+});
+
+router.delete('/logout', function (req, res, next) {
+    req.session.destroy();
+    res.sendStatus(204);
 });
 
 module.exports = router;
