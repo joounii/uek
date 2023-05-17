671a86df30cd8d0c6904de827b2d842d09b681d4
jonathan <jonifu06@gmail.com>
2023-05-17 09:52:27 +0200

    added post to tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-const tasks = [{ id: 1, title: 'finish homework', createdAt: '2023-03-19T23:15:30.000Z', finishedAt: '2023-04-02T19:15:30.000Z' }, { id: 1, title: 'do the dishes', createdAt: '2023-05-01T06:36:30.000Z', finishedAt: '' }];
+const tasks = [{ id: 1, title: 'finish homework', createdAt: 'Wed May 01 2023 09:44:18 GMT+0200 (Central European Summer Time)', finishedAt: 'Wed May 02 2023 09:44:18 GMT+0200 (Central European Summer Time)' }, { id: 2, title: 'do the dishes', createdAt: 'Wed May 16 2023 09:44:18 GMT+0200 (Central European Summer Time)', finishedAt: '' }];
+let highestId = 2;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.json(tasks);
+});
+
+router.post('/', function (req, res, next) {
+    const { title } = req.body;
+    highestId++;
+    const id = highestId;
+    const createdAt = Date();
+    const finishedAt = '';
+
+    let task = {};
+    if (title) {
+        task = {
+            id,
+            title,
+            createdAt,
+            finishedAt
+        };
+        tasks.push(task);
+    } else {
+        res.status(400).send('no title exists');
+    }
+    res.status(201).json(task);
 });
 
 module.exports = router;
